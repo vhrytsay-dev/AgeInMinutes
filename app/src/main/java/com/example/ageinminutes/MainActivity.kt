d53270711894bd0c4671aaa68457a4f09ce5f53f
Viktor Hrytsay <viktor2@uni-bremen.de>
2022-05-09 22:04:52 +0200

    Add UI Elements

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener{
+            view -> onClickDatePicker(view)
+        }
+    }
+    
+    fun onClickDatePicker(view: View){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val dayOfMonth = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener
+            { view, year, month, dayOfMonth -> }
+            ,year, month, dayOfMonth).show()
     }
 }
